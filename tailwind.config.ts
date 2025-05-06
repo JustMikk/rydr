6c5a6cc98f4613b94c1117c2a26e03831ab51386
Mikiyas Hailegebriel <mikiyasbutu@gmail.com>
2025-05-06 07:20:18 +0300

    Add primary and secondary color palettes to Tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,28 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
+        primary: {
+          100: "#D6E4FD",
+          200: "#AEC8FC",
+          300: "#85A8F8",
+          400: "#658DF1",
+          500: "#3563E9",
+          600: "#264BC8",
+          700: "#1A37A7",
+          800: "#102587",
+          900: "#0A196F",
+        },
+        secondary: {
+          100: "#E0E9F4",
+          200: "#C3D4E9",
+          300: "#90A3BF",
+          400: "#596780",
+          500: "#1A202C",
+          600: "#131825",
+          700: "#0D121F",
+          800: "#080C19",
+          900: "#040815",
+        },
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
